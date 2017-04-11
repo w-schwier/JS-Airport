2ff08bd55046d121d9ab6eb81a58d97ae331710e
Will Schwier <w.schwier@hotmail.co.uk>
2017-04-11 15:49:34 +0100

    Add failing test for isFlying default value

--- spec/planeSpec.js
+++ spec/planeSpec.js
@@ -1,13 +1,22 @@
-describe ('Plane', function() { 
+describe ('Plane', function() {
   var plane;
 
   beforeEach(function() {
     plane = new Plane();
   });
+
+  describe('is flying', function() {
+    it('set to false by default', function() {
+      expect(plane.isFlying).toBe(false)
+    });
+  });
   describe('take off', function() {
-      it ('', function() {
-        spyOn(plane, 'takeOff')
-        expect(plane.takeOff).toHaveBeenCalled();
-      });
+    it ('should make plane fly', function() {
+      plane.takeOff()
+      expect(plane.isFlying).toBe(true);
+    });
+    it('should throw error if plane is already flying', function() {
+
+    });
   });
 });
---
+++ src/plane.js
@@ -0,0 +1,12 @@
+function Plane() {
+  // this.isFlying = function(){
+  //
+  // };
+  // this.isFlying() = false
+}
+
+
+
+Plane.prototype.takeOff = function () {
+  this.isFlying = true;
+};
